2c17a8460de0c61d6ca5dc2c6716c423b757ecdb
Vladimir Druzhinin <vlado@status.im>
2023-11-21 19:46:36 +0100

    chore: generate timestamp for nightly run
    #320

--- configs/testrail.py
+++ configs/testrail.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 CI_BUILD_URL = os.getenv('BUILD_URL', '')
 
@@ -7,3 +8,7 @@ PROJECT_ID = os.getenv('TESTRAIL_PROJECT_ID', '')
 URL = os.getenv('TESTRAIL_URL', '')
 USR = os.getenv('TESTRAIL_USR', '')
 PSW = os.getenv('TESTRAIL_PSW', '')
+
+# Because of an issue with generating a timestamp on the CI side and adding it as a parameter on the test job:
+if RUN_NAME.strip() == 'Nightly regression':
+    RUN_NAME += f' {datetime.now():%Y.%m.%d}'
