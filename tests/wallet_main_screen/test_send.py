8ff5c034f0efd09a043dbb575bd8333112aaa6d2
Valentina Novgorodtceva <valentina@status.im>
2024-01-25 14:18:24 +0700

    draft for transaction tests (user data, commandline - for build with edited configs)

--- tests/wallet_main_screen/test_send.py
+++ tests/wallet_main_screen/test_send.py
@@ -1,3 +1,5 @@
+import subprocess
+
 import allure
 import pytest
 from allure_commons._allure import step
@@ -11,6 +13,7 @@ from gui.components.onboarding.beta_consent_popup import BetaConsentPopup
 from gui.components.signing_phrase_popup import SigningPhrasePopup
 from gui.components.splash_screen import SplashScreen
 from gui.components.wallet.authenticate_popup import AuthenticatePopup
+from gui.main_window import MainWindow
 from gui.screens.onboarding import KeysView, AllowNotificationsView, WelcomeToStatusView, BiometricsView
 
 
@@ -31,7 +34,7 @@ def keys_screen(main_window) -> KeysView:
 @pytest.mark.parametrize('receiver_account_address, amount, asset', [
     pytest.param(constants.user.user_account_one.status_address, 0, 'Ether')
 ])
-@pytest.mark.xfail(reason="https://github.com/status-im/status-desktop/issues/12987")
+# @pytest.mark.xfail(reason="https://github.com/status-im/status-desktop/issues/12987")
 def test_wallet_send_0_eth(keys_screen, main_window, user_account, receiver_account_address, amount, asset):
     with step('Open import seed phrase view and enter seed phrase'):
         input_view = keys_screen.open_import_seed_phrase_view().open_seed_phrase_input_view()
@@ -79,3 +82,41 @@ def test_wallet_send_0_eth(keys_screen, main_window, user_account, receiver_acco
     with step('Verify toast message with Transaction pending appears'):
         assert WalletTransactions.TRANSACTION_PENDING_TOAST_MESSAGE.value in ' '.join(
             main_window.wait_for_notification())
+
+
+@allure.testcase('https://ethstatus.testrail.net/index.php?/cases/view/704527',
+                 'Send: can send 0 ETH to address pasted into receiver field with Simple flow')
+@pytest.mark.case(704527)
+@pytest.mark.parametrize('user_account', [constants.user.user_with_funds])
+@pytest.mark.parametrize('user_data', [configs.testpath.TEST_USER_DATA / 'user_with_funds'])
+@pytest.mark.parametrize('receiver_account_address, amount, asset', [
+    pytest.param(constants.user.user_account_one.status_address, 0, 'Ether')
+])
+def test_wallet_send_0_eth_anvil(main_screen: MainWindow, user_account, receiver_account_address, amount, asset):
+    command = f'anvil --fork-url https://eth-goerli.g.alchemy.com/v2/YxOYtKMn54Glm74M6IsBWgxMg1qi7s_F --fork-block-number 10352254 --fork-chain-id 5 --chain-id 5'
+    process = subprocess.Popen(command, shell=True, stderr=subprocess.STDOUT, stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE)
+
+    wallet_settings = main_screen.left_panel.open_settings().left_panel.open_wallet_settings()
+
+    with step('Set testnet mode'):
+        wallet_settings.open_networks().switch_testnet_mode_toggle().turn_on_testnet_mode_in_testnet_modal()
+
+    with step('Open send popup'):
+        wallet = main_screen.left_panel.open_wallet()
+        SigningPhrasePopup().wait_until_appears().confirm_phrase()
+        assert driver.waitFor(lambda: wallet.left_panel.is_total_balance_visible, configs.timeouts.UI_LOAD_TIMEOUT_SEC)
+        f"Total balance is not visible"
+        wallet_account = wallet.left_panel.select_account('Status account')
+        send_popup = wallet_account.open_send_popup()
+
+    with step('Enter asset, amount and address and click send and verify Mainnet network is shown'):
+        send_popup.send(receiver_account_address, amount, asset)
+        assert driver.waitFor(lambda: send_popup.is_mainnet_network_identified, configs.timeouts.UI_LOAD_TIMEOUT_SEC)
+
+    # with step('Enter password in authenticate popup'):
+    #     AuthenticatePopup().wait_until_appears().authenticate(user_account.password)
+
+    with step('Verify toast message with Transaction pending appears'):
+        assert WalletTransactions.TRANSACTION_PENDING_TOAST_MESSAGE.value in ' '.join(
+            main_screen.wait_for_notification())
