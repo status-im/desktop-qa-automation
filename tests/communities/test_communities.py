0014c224266f68458a191264efbaa67c0ee64e4a
Valentina Novgorodtceva <valentina@status.im>
2024-05-03 18:11:17 +0200

    chore: added steps of enabling of create community button to all needed tests

--- tests/communities/test_communities.py
+++ tests/communities/test_communities.py
@@ -92,6 +92,9 @@ def test_create_community(user_account, main_screen: MainWindow, params):
 ])
 @pytest.mark.skip(reason="https://github.com/status-im/status-desktop/issues/13783")
 def test_edit_community(main_screen: MainWindow, params):
+    with step('Enable creation of community option'):
+        settings = main_screen.left_panel.open_settings()
+        settings.left_panel.open_advanced_settings().enable_creation_of_communities()
     community_params = constants.community_params
     main_screen.create_community(community_params['name'], community_params['description'],
                                  community_params['intro'], community_params['outro'],
@@ -171,6 +174,10 @@ def test_community_admin_kick_member_and_delete_message(multiple_instances):
             contacts_settings.accept_contact_request(user_one.name)
 
         with step(f'User {user_two.name}, create community and invite {user_one.name}'):
+            with step('Enable creation of community option'):
+                settings = main_screen.left_panel.open_settings()
+                settings.left_panel.open_advanced_settings().enable_creation_of_communities()
+
             main_screen.create_community(community_params['name'], community_params['description'],
                                          community_params['intro'], community_params['outro'],
                                          community_params['logo']['fp'], community_params['banner']['fp'])
